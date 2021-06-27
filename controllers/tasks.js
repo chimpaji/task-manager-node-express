aebafa23b6d5e250e83a366c555f4b1b7da741a9
chimpaji <chimpaji@outlook.com>
2021-06-28 01:08:57 +0700

    finish crud

--- controllers/tasks.js
+++ controllers/tasks.js
@@ -1,25 +1,67 @@
 const Task = require("../models/tasks");
 
-const getAllTasks = (req, res) => {
+const getAllTasks = async (req, res) => {
+  try {
+    const tasks = await Task.find({});
+    res.status(200).json({ tasks });
+  } catch (error) {
+    res.status(500).json({ msg: error });
+  }
   res.send("get all tasks");
 };
 
 const createTask = async (req, res) => {
-  const task = await Task.create(req.body);
-  res.status(200).json({task});
+  try {
+    const task = await Task.create(req.body);
+    res.status(200).json({ task });
+  } catch (error) {
+    res.status(500).json({ msg: error });
+  }
 };
 
-const getTask = (req, res) => {
+const getTask = async (req, res) => {
   const { taskID } = req.params;
-  res.send(`get single task ${taskID}`);
+
+  try {
+    const task = await Task.findOne({ _id: taskID });
+    //right patter of taskID provided but not found task we have to handle this error by ourself
+    if (!task) {
+      return res.status(404).json({ msg: `No task with id: ${taskID}` });
+    }
+    res.status(201).json({ task: task });
+  } catch (error) {
+    //will catch error with the wrong pattern of inputed taskID only,
+    res.status(500).json({ mgs: error });
+  }
 };
 
-const updateTask = (req, res) => {
-  res.send(`update a task ${req.params.taskID}`);
+const deleteTask = async (req, res) => {
+  try {
+    const task = await Task.findOneAndDelete({ _id: req.params.taskID });
+    if (!task) {
+      return res.status(400).json({ msg: `not found task with id: ${taskID}` });
+    }
+    res.status(200).json({ task });
+  } catch (error) {
+    res.status(500).json({ msg: error });
+  }
 };
 
-const deleteTask = (req, res) => {
-  res.send("delete a task");
+const updateTask = async (req, res) => {
+  try {
+    const { taskID } = req.params;
+    //can add option object in findOneAndUpdate so we can tell user to MUST provide "name" with new update
+    const task = await Task.findOneAndUpdate({ _id: taskID }, req.body, {
+      new: true,
+      runValidators: true,
+    });
+    if (!task) {
+      return res.status(400).json({ msg: `Not found task id: ${taskID}` });
+    }
+    res.status(200).json({ id: { taskID }, data: req.body });
+  } catch (error) {
+    res.status(500).json({ msg: error });
+  }
 };
 
 module.exports = { getAllTasks, createTask, getTask, updateTask, deleteTask };
