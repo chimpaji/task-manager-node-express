701d250a6fc13063f16c8d3a9e9308c4cf8929d3
chimpaji <chimpaji@outlook.com>
2021-06-28 11:34:42 +0700

    connect to react

--- controllers/tasks.js
+++ controllers/tasks.js
@@ -58,7 +58,7 @@ const updateTask = async (req, res) => {
     if (!task) {
       return res.status(400).json({ msg: `Not found task id: ${taskID}` });
     }
-    res.status(200).json({ id: { taskID }, data: req.body });
+    res.status(200).json({ task });
   } catch (error) {
     res.status(500).json({ msg: error });
   }
